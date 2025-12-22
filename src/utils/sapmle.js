655d92560463a3d1e7691d57a667d84671ea5cc7
Tk sultan <Connect.thrivewellness@gmail.com>
2025-12-22 14:56:57 +0530

    auto deply file delte

--- src/utils/sapmle.js
+++
@@ -1,8 +0,0 @@
-export default function errorHandler(err, req, res, next) {
-  console.error(err.stack);
-
-  res.status(500).json({
-    success: false,
-    message: 'Internal Server Error'
-  });
-}
