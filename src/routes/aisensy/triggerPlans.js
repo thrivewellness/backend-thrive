7ddd4a4e3556b1406a3d25e5760a2cbf66b6d2b7
Tk sultan <Connect.thrivewellness@gmail.com>
2026-08-18 22:38:42 +0530

    day 4

--- src/routes/aisensy/triggerPlans.js
+++ src/routes/aisensy/triggerPlans.js
@@ -65,7 +65,7 @@ export const triggerPlans = async (dayNumber) => {
       }
 
       try {
-        await sendChineseMsg(id, whatsappPhone, name, dayNumber);
+        await sendPlansOfferMsg(id, whatsappPhone, name, dayNumber);
         sentCount++;
       } catch (err) {
         console.error(`> Failed for user ${id}:`, err.message);
